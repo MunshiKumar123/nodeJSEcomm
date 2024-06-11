2824942d4f11e126d5046dcbc9281d360f78f831
Munshi Kumar <106418773+MunshiKumar123@users.noreply.github.com>
2024-06-11 15:30:14 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const cors = require("cors")
 require('./db/config')
 const app = express()
 app.use(express.json())
-app.use(cors())
+app.use(cors({
+  credentials: true,
+  origin: "node-js-ecomm-mh0qv0svg-munshitesting101gmailcoms-projects.vercel.app",
+  allowedHeaders: ["Content-Type", "Authorization", "other-header"]
+}));
 
 const user = require('./_routes/user');
 const product = require('./_routes/product');
